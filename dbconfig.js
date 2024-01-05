47ac35f28b962054afedf0f2cd7c6b3e65bf8f6c
Jerry Nguyen Dinh <jdinh14@Administrators-MacBook-Pro.local>
2024-01-05 14:31:58 -0500

    header footer

--- dbconfig.js
+++ dbconfig.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const connection = mysql.createConnection ({
-    host: 'localhost',
-    user: "root",
-    password: "password",
-    database: 'carbonEmissionCities',
-    port: 3306,
-  });
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: "root",
+//   password: "password",
+//   database: 'carbonEmissionCities',
+//   port: 3306,
+// });
 
-  
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL:', err);
-      return;
-    }
-    console.log('Connected to MySQL database');
-  });
 
-  module.exports = connection;
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL database');
+// });
+
+// module.exports = connection;
