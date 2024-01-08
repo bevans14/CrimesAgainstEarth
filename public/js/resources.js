35b6e2e3212121a77eaf7e66df43d55662f8b645
TommyPhi <tommy.phi04@gmail.com>
2024-01-08 15:41:00 -0500

    added pop cities

---
+++ public/js/resources.js
@@ -0,0 +1,16 @@
+const list = document.querySelector('ul');
+
+fetch(`http://localhost:3000/popularCity`).then((response) => {
+    response.json().then((data) => {
+        data.forEach(location => {
+            cityElement = document.createElement('li')
+            cityElement.innerHTML = `
+            <div>
+                <h1>${location.city}, ${location.state}, ${location.country}</h1>
+                <p>Current Air Quality Index: ${location.airQual.pollution.aqius}</p>
+            <div>
+            `
+            list.appendChild(cityElement)
+        })
+    })
+})
